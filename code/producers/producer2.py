6cc02f9b86c85ea5a6220a9c5526024fb116a556
AleksiSupikoski <54030510+AleksiSupikoski@users.noreply.github.com>
2023-04-14 02:43:01 +0300

    upload

--- code/producers/producer2.py
+++ code/producers/producer2.py
@@ -14,6 +14,11 @@ for i in range(100000):
         data = {
         'wrong_device': 'absolutely wrong data'
         }
+    if i == 3: # emulate "wrong data"
+        data2 = {
+        'wrong_device': 'absolutely wrong data'
+        }
+
     else:
         data = { # <----- sometimes rand's will generate unsuitable environment for the tortoise
             'time': calendar.timegm(time.gmtime()),
@@ -28,6 +33,20 @@ for i in range(100000):
             'temperature': round(random.uniform(15,31),2),
             'dev_id': 'C4:1G:1A:C2:42:12'
             }
+        data2 = { # <----- sometimes rand's will generate unsuitable environment for the tortoise
+            'time': calendar.timegm(time.gmtime()),
+            'readable_time' : datetime.datetime.now().isoformat(),
+            'acceleration' : round(random.uniform(0,1000),5),
+            'acceleration_x': round(random.uniform(-1000,1000),1),
+            'acceleration_y': round(random.uniform(-1000,1000),1),
+            'acceleration_z': round(random.uniform(-1000,1000),1),
+            'battery': random.randint(0,100),
+            'humidity': random.randint(97,100),
+            'pressure': round(random.uniform(999,1005),2),
+            'temperature': round(random.uniform(15,31),2),
+            'dev_id': 'C2:11:1B:C1:42:12'
+            }
     producer.send('tenant2-data', json.dumps(data, default=json_util.default).encode('utf-8'))
+    producer.send('tenant2-data', json.dumps(data2, default=json_util.default).encode('utf-8'))
 
     #time.sleep(5) # turn off for extremely fast data speed.
