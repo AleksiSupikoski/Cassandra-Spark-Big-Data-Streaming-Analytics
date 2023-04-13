9c659fddb2a9ec0bd6700fbfce4f257004df9c0a
AleksiSupikoski <54030510+AleksiSupikoski@users.noreply.github.com>
2023-04-14 02:41:06 +0300

    Create producer2.py

---
+++ code/producers/producer2.py
@@ -0,0 +1,33 @@
+import json
+import time
+import calendar
+import datetime
+import random
+from bson import json_util
+from kafka import KafkaProducer
+
+
+producer = KafkaProducer(bootstrap_servers='localhost:9093')
+
+for i in range(100000):
+    if i == 3: # emulate "wrong data"
+        data = {
+        'wrong_device': 'absolutely wrong data'
+        }
+    else:
+        data = { # <----- sometimes rand's will generate unsuitable environment for the tortoise
+            'time': calendar.timegm(time.gmtime()),
+            'readable_time' : datetime.datetime.now().isoformat(),
+            'acceleration' : round(random.uniform(0,1000),5),
+            'acceleration_x': round(random.uniform(-1000,1000),1),
+            'acceleration_y': round(random.uniform(-1000,1000),1),
+            'acceleration_z': round(random.uniform(-1000,1000),1),
+            'battery': random.randint(0,100),
+            'humidity': random.randint(97,100),
+            'pressure': round(random.uniform(999,1005),2),
+            'temperature': round(random.uniform(15,31),2),
+            'dev_id': 'C4:1G:1A:C2:42:12'
+            }
+    producer.send('tenant2-data', json.dumps(data, default=json_util.default).encode('utf-8'))
+
+    #time.sleep(5) # turn off for extremely fast data speed.
